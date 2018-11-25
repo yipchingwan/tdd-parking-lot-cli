9341de105f0dff4a3d0576df126e4f4eb2930895
KYLE\Surface <kyle.public10415@gmail.com>
2018-11-25 18:18:36 +0800

    passed test cases to fufil AC1

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -11,12 +11,14 @@ public class ParkingBoy {
 
     public ParkingTicket park(Car car) {
         // TODO: Please implement the method
-        throw new RuntimeException("Not implemented");
+        ParkingTicket ticket = new ParkingTicket(car);
+        return ticket;
     }
 
     public Car fetch(ParkingTicket ticket) {
         // TODO: Please implement the method
-        throw new RuntimeException("Not implemented");
+        Car car = ticket.getCar();
+        return car;
     }
 
     public String getLastErrorMessage() {
--- src/main/java/com/oocl/cultivation/ParkingTicket.java
+++ src/main/java/com/oocl/cultivation/ParkingTicket.java
@@ -1,4 +1,19 @@
 package com.oocl.cultivation;
 
 public class ParkingTicket {
+    private Car car;
+
+    public ParkingTicket() {
+        this.car = null;
+    }
+
+    public ParkingTicket(Car car) {
+        this.car = car;
+    }
+
+    public Car getCar(){
+        return this.car;
+    }
+
+
 }
